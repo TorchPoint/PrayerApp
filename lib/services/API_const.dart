4c465d274f43fdd061bfddab7d661639ee9cf3ca
Alishan <ali.shan@techando.co>
2021-09-03 07:07:57 -0700

    p

---
+++ lib/services/API_const.dart
@@ -0,0 +1,19 @@
+class ApiConst {
+  static const BASE_URL = "https://server.appsstaging.com/3048/prayer/api/";
+
+  //------- Registration Module URL--------//
+
+  static const SIGN_IN_URL = "login";
+  static const FORGET_PASSWORD_URL = "forgot-password";
+  static const FORGET_VERIFICATION_URL = "forgot-password-otp-verify";
+  static const RESEND_FORGET_VERIFICATION_URL = "forgot-password";
+  static const RESET_PASSWORD_URL = "reset-forgot-password";
+  static const SIGNUP_URL = "signup";
+  static const VERIFICATION_URL = "verification";
+  static const RESEND_EMAIL_VERIFICATION_URL = "resend-otp";
+  static const UPDATE_PROFILE = "update-profile";
+  static const CONTENT_URL = "contents";
+  static const LOGOUT_URL = "logout";
+  static const UPDATE_PASSWORD_URL = "change-password";
+  static const HELP_FEEDBACK_URL = "add-feedback";
+}
