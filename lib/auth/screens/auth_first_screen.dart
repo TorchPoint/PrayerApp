8d19136a410566d7be3c03c25a5c24e3d58a16dc
Alishan <ali.shan@techando.co>
2021-09-07 06:24:53 -0700

    ALIGNEMNT

--- lib/auth/screens/auth_first_screen.dart
+++ lib/auth/screens/auth_first_screen.dart
@@ -4,9 +4,9 @@ import 'package:prayer_hybrid_app/utils/app_strings.dart';
 import 'package:prayer_hybrid_app/utils/asset_paths.dart';
 import 'package:prayer_hybrid_app/widgets/custom_button.dart';
 
-
 class AuthFirstScreen extends StatefulWidget {
   final PageController pageController;
+
   AuthFirstScreen({this.pageController});
 
   @override
@@ -19,21 +19,34 @@ class _AuthFirstScreenState extends State<AuthFirstScreen> {
     return SingleChildScrollView(
       child: Column(
         children: [
-         Padding(
-           padding: EdgeInsets.only(top:MediaQuery.of(context).size.height*0.13),
-           child: Text(AppStrings.WELCOME_TO_TEXT,style: TextStyle(color: AppColors.WHITE_COLOR,fontWeight: FontWeight.w600,letterSpacing: 1.5),textScaleFactor: 1.6,),
-         ),
-
-         _prayerImageWidget(),
-
-          Container(
-            width: MediaQuery.of(context).size.width*0.75,
-            margin: EdgeInsets.only(top: 10.0),
-            child: Text(AppStrings.GET_STARTED_TEXT,style: TextStyle(color: AppColors.WHITE_COLOR,fontWeight: FontWeight.w700,letterSpacing: 1.0),textScaleFactor: 1.1,textAlign: TextAlign.center,),
+          Padding(
+            padding:
+                EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.13),
+            child: Text(
+              AppStrings.WELCOME_TO_TEXT,
+              style: TextStyle(
+                  color: AppColors.WHITE_COLOR,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 1.5),
+              textScaleFactor: 1.6,
+            ),
           ),
-
+          _prayerImageWidget(),
           Container(
-            width: MediaQuery.of(context).size.width*0.81,
+            width: MediaQuery.of(context).size.width * 0.75,
+            margin: EdgeInsets.only(top: 10.0),
+            child: Text(
+              AppStrings.GET_STARTED_TEXT,
+              style: TextStyle(
+                  color: AppColors.WHITE_COLOR,
+                  fontWeight: FontWeight.w700,
+                  letterSpacing: 1.0),
+              textScaleFactor: 1.1,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.81,
             margin: EdgeInsets.only(top: 35.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -46,31 +59,26 @@ class _AuthFirstScreenState extends State<AuthFirstScreen> {
           SizedBox(
             height: 5.0,
           )
-
         ],
       ),
     );
   }
 
-  Widget _prayerImageWidget()
-  {
+  Widget _prayerImageWidget() {
     return Container(
-      width: MediaQuery.of(context).size.width*0.58,
-      height: MediaQuery.of(context).size.height*0.15,
-      margin: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.04),
+      width: MediaQuery.of(context).size.width * 0.58,
+      height: MediaQuery.of(context).size.height * 0.15,
+      margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.04),
       decoration: BoxDecoration(
           image: DecorationImage(
               image: AssetImage(AssetPaths.FOREGROUND_IMAGE),
-              fit: BoxFit.contain
-          )
-      ),
+              fit: BoxFit.contain)),
     );
   }
 
-  Widget _loginButtonWidget()
-  {
+  Widget _loginButtonWidget() {
     return CustomButton(
-      containerWidth: MediaQuery.of(context).size.width*0.38,
+      containerWidth: MediaQuery.of(context).size.width * 0.38,
       buttonColor: AppColors.WHITE_COLOR,
       borderColor: AppColors.WHITE_COLOR,
       elevation: true,
@@ -80,18 +88,16 @@ class _AuthFirstScreenState extends State<AuthFirstScreen> {
       fontSize: 1.27,
       paddingTop: 11.0,
       paddingBottom: 11.0,
-      onTap: (){
-      print("login");
-     widget.pageController.jumpToPage(4);
+      onTap: () {
+        print("login");
+        widget.pageController.jumpToPage(4);
       },
     );
   }
 
-
-  Widget _signUpButtonWidget()
-  {
+  Widget _signUpButtonWidget() {
     return CustomButton(
-      containerWidth: MediaQuery.of(context).size.width*0.38,
+      containerWidth: MediaQuery.of(context).size.width * 0.38,
       buttonColor: AppColors.TRANSPARENT_COLOR,
       borderColor: AppColors.WHITE_COLOR,
       buttonText: AppStrings.SIGN_UP_TEXT,
@@ -100,11 +106,10 @@ class _AuthFirstScreenState extends State<AuthFirstScreen> {
       fontSize: 1.27,
       paddingTop: 11.0,
       paddingBottom: 11.0,
-      onTap: (){
+      onTap: () {
         print("SignUp");
         widget.pageController.jumpToPage(3);
       },
     );
   }
-
 }
