6a69a5efe082b01477201fdaaa93bef2a7f7f79c
Alishan <ali.shan@techando.co>
2021-09-21 05:29:52 -0700

    APIS BIBLE PROMISES COMPLETED

--- lib/services/base_service.dart
+++ lib/services/base_service.dart
@@ -714,6 +714,7 @@ class BaseService {
     });
   }
 
+
   Future finishPrayer(BuildContext context, prayerID, prayerDuration) async {
     Map<String, String> requestBody = <String, String>{
       "prayer": prayerID.toString(),
